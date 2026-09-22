08c4b1cef850e1536672d4ecf0406372e5766494
Lucas Eduardo <lucaseduardomeyer2007@gmail.com>
2026-09-21 22:15:45 -0300

    Feito

--- api/app/main.py
+++ api/app/main.py
@@ -3,9 +3,18 @@ from app.core.exceptions import registrar_handler
 from app.controllers.usuario_controller import router as usuario_router
 from app.controllers.categoria_controller import router as categoria_router
 from app.controllers.ticket_controller import router as ticket_router
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Traduz as exceções de domínio (app/core/exceptions.py) para respostas HTTP padronizadas
 registrar_handler(app)
