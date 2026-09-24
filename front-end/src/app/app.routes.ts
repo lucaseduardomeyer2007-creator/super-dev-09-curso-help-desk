f0402af2e840e28012c88eeb2759669b44e9acf0
Lucas <lucaseduardomeyer2007@gmail.com>
2026-09-23 22:05:27 -0300

    aulas

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -4,5 +4,9 @@ export const routes: Routes = [
     {
         path: "tickets",
         loadComponent:() => import("./pages/tickets/listar/listar").then(m => m.Listar)
+    },
+    {
+        path: "tickets/cadastro",
+        loadComponent:() => import("./pages/tickets/cadastro/cadastro").then(m => m.Cadastro)
     }
 ];
---
+++ front-end/src/app/pages/tickets/cadastro/cadastro.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-cadastro',
+  styleUrl: './cadastro.scss',
+  templateUrl: './cadastro.html',
+})
+export class Cadastro {}
